ec648fe5b286928624e706f419423047f79d2503
YeHtut-kyaw <yehtutkyaw@megliofuturo.co.jp>
2020-03-23 14:53:15 +0630

    added all code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from 'react';
+import Style from './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class Hello extends Component
+{
+  render(){
+    return <div>Hello {this.props.name}!</div>
+  }
+}
+
+class App extends Component
+{
+  constructor() {
+    super()
+    this.state = {
+      name: null
+    }
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(e) {
+    this.setState({ name: e.target.value })
+  }
+
+  render(){
+    return <div>
+      <Hello name={this.state.name}/>
+      <input type="text" onKeyDown={this.handleChange}/>
     </div>
-  );
+  }
 }
 
 export default App;
